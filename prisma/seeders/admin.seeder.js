4454b6a670edf5ac1f3646e3bdece104391f88d0
Anu karnan <anukarnan24@gmail.com>
2026-02-05 00:09:28 +0530

    feat: seeder to creat admin account

---
+++ prisma/seeders/admin.seeder.js
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+async function main() {
+  const { PrismaClient } = pkg;
+
+  const globalForPrisma = globalThis;
+  const connectionString = `${process.env.DATABASE_URL}`;
+  const adapter = new PrismaPg({ connectionString });
+
+  const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+  const email = "admin@crm.local";
+  const password = "admin123"; // change after first login
+
+  const existing = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existing) {
+    console.log("✅ Admin user already exists:", email);
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const admin = await prisma.user.create({
+    data: {
+      name: "Admin",
+      email,
+      password: hashedPassword,
+      role: "ADMIN",
+    },
+  });
+
+  console.log("🚀 Admin user created");
+  console.log({
+    email: admin.email,
+    password,
+    role: admin.role,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error("❌ Seeder failed:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
